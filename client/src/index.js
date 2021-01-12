8dadd2c495b1fb6031ec5c285297c42334fbf0d9
misheMatcha <mishe.vong@gmail.com>
2021-01-12 15:56:52 -0800

    add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
